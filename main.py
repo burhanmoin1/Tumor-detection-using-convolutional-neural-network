8d1ce904a784c0a9a2026d38ba6b69dd96e0720a
Burhan <m.burhanmoin1@gmail.com>
2022-10-30 02:03:23 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,110 +1,65 @@
-import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler, LabelEncoder
-
-import skimage.io as io
-import os
-
+import numpy as np
+import PIL
 import tensorflow as tf
-from tensorflow import keras
-from keras.utils import normalize, to_categorical
-from keras.layers import Conv2D, Dropout, Flatten, Activation, MaxPooling2D, Dense
-from keras.models import Sequential
-
-import cv2 as cv
-from skimage import data, color
+from tensorflow.keras import layers
+from tensorflow.python.keras.layers import Dense,Flatten
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.optimizers import Adam
+import os
+import cv2
+import skimage.io as io
 from skimage.transform import rescale, resize
 
+data_dir = 'C:\\Users\\SAR Computers\\Documents\\datasets'
 
-from PIL import Image
-dataset = []
-label = []
-path = 'C:\\Users\\SAR Computers\\Documents\\datasets\\meni'
-meni_tumor_images = os.listdir(path)
+img_height,img_width=150,150
+batch_size=16
+train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+  data_dir,
+  validation_split=0.2,
+  subset="training",
+  seed=123, 
+    label_mode='categorical',
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
 
-for image_name in meni_tumor_images:
-    img = io.imread(os.path.join(path, image_name), as_gray=True)
-    img = resize(img, (200,200),
-                       anti_aliasing=True)
-    dataset.append(np.array(img))
-    label.append("Meningioma")
-    
-    path_two = 'C:\\Users\\SAR Computers\\Documents\\datasets\\no'
-no_tumor_images = os.listdir(path_two)
+val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+  data_dir,
+  validation_split=0.2,
+  subset="validation",
+  seed=123, label_mode='categorical',
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
 
-for image_name in no_tumor_images:
-    img = io.imread(os.path.join(path_two, image_name), as_gray=True)
-    img = resize(img, (200,200),
-                       anti_aliasing=True)
-    dataset.append(np.array(img))
-    label.append("No tumor")
-    
-    path_three = 'C:\\Users\\SAR Computers\\Documents\\datasets\\gli'
-gli_tumor_images = os.listdir(path_three)
-
-for image_name in gli_tumor_images:
-    img = io.imread(os.path.join(path_three, image_name), as_gray=True)
-    img = resize(img, (200,200),
-                       anti_aliasing=True)
-    dataset.append(np.array(img))
-    label.append("Glioma")
-    
-    
-path_four = 'C:\\Users\\SAR Computers\\Documents\\datasets\\piti'
-piti_tumor_images = os.listdir(path_four)
-
-for image_name in piti_tumor_images:
-    img = io.imread(os.path.join(path_four, image_name), as_gray=True)
-    img = resize(img, (200,200),
-                       anti_aliasing=True)
-    dataset.append(np.array(img))
-    label.append("Pitiuary")
-    
-dataset = np.array(dataset)
-label = np.array(label)
+class_names = train_ds.class_names
+print(class_names)
 
 
-le = LabelEncoder()
-label = le.fit_transform(label)
-label
+resnet_model = Sequential()
 
-X_train, X_test, y_train, y_test = train_test_split(dataset, label, test_size=0.25, random_state=42)
+pretrained_model= tf.keras.applications.ResNet50(include_top=False,
+                   input_shape=(150,150,3),
+                   pooling='avg',classes=4,
+                   weights='imagenet')
+for layer in pretrained_model.layers:
+        layer.trainable=False
 
-X_train = normalize(X_train)
-X_test = normalize(X_test)
-X_train
-#to categorical label
+resnet_model.add(pretrained_model)
+resnet_model.add(Flatten())
+resnet_model.add(Dense(512, activation='relu'))
+resnet_model.add(Dense(len(class_names), activation='softmax'))
+resnet_model.summary()
+
+resnet_model.compile(optimizer=Adam(learning_rate=0.001),loss='categorical_crossentropy',metrics=['accuracy'])
+
+history = resnet_model.fit(train_ds, validation_data=val_ds, epochs=10)
+
+image=cv2.imread('C:\\Users\\SAR Computers\\Documents\\datasets\\gli\\2720.png')
+image_resized= cv2.resize(image, (img_height,img_width))
+image=np.expand_dims(image_resized,axis=0)
+pred=resnet_model.predict(image)
 
 
-model = Sequential()
-
-model.add(Conv2D(filters= 16,kernel_size= (3,3), input_shape=(200,200,1), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2,2)))
-​
-model.add(Conv2D(32, (3,3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2,2)))
-​
-model.add(Conv2D(64, (3,3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2,2)))
-​
-model.add(Flatten())
-model.add(Dense(64))
-model.add(Activation('relu'))
-model.add(Dropout(0.5))
-model.add(Dense(1))
-model.add(Activation('sigmoid'))
-​
-model.compile(loss = 'binary_crossentropy',
-             optimizer='adam',
-             metrics=['accuracy'])
-
-model.fit(X_train, y_train, batch_size=16, epochs=10,verbose=1,validation_data=(X_test, y_test),shuffle=False)
-
-print("Generate predictions for the 3 samples from ismail")
-predictions = model.predict(yes_testing[:6])
-predictions
+output_class=class_names[np.argmax(pred)]
+print("The predicted class is", output_class)
